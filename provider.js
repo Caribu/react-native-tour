6034af08f3154c8343675c52afdcdc71d7653132
Zach Silveira <zackify@gmail.com>
2018-09-17 16:55:40 -0700

    Hide the overlay when waiting for user input

--- provider.js
+++ provider.js
@@ -29,21 +29,24 @@ export default class OnboardingProvider extends React.Component {
     this.state = { positions: {}, step: initialStep, steps };
   }
   render() {
-    const { step, steps, positions } = this.state;
+    const { step, steps, positions, hideOverlay } = this.state;
     let activeStep = steps[step] || {};
     const { style, overlay } = positions[activeStep.name] || {};
     // if the active step isnt on screen yet, we cant try to render anything
     if (!style) activeStep = {};
 
-    const next = async () => {
+    const next = async (data = {}) => {
+      const { hideOverlay } = data;
+
       const nextStep = steps[step + 1];
       if (nextStep && nextStep.beforeStep) await nextStep.beforeStep();
 
       this.setState({
+        hideOverlay,
         step: steps[step + 1] ? step + 1 : null,
       });
     };
-
+    console.log(activeStep, hideOverlay);
     return (
       <Provider
         value={{
@@ -59,7 +62,7 @@ export default class OnboardingProvider extends React.Component {
             this.setState({ step: 0 });
           },
           onLayout: (name, data) =>
-            this.setState({ positions: { [name]: data } }),
+            this.setState({ positions: { [name]: data }, hideOverlay: false }),
           setActive: stepName =>
             this.setState({
               step: steps.findIndex(row => row.name === stepName),
@@ -68,12 +71,25 @@ export default class OnboardingProvider extends React.Component {
       >
         <View style={styles.container}>
           {this.props.children}
-          {step || step === 0 ? (
+          {(step || step === 0) && !hideOverlay ? (
             <View style={styles.overlay}>
               {activeStep.name ? (
                 <Fade>
-                  <View style={[styles.highlight, style]}>{overlay}</View>
-                  <View style={{ top: style.top, left: style.left }}>
+                  {overlay ? (
+                    <View style={[styles.highlight, style]}>{overlay}</View>
+                  ) : null}
+                  <View
+                    style={
+                      overlay
+                        ? { top: style.top, left: style.left }
+                        : {
+                            display: 'flex',
+                            zIndex: 10,
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                          }
+                    }
+                  >
                     {React.createElement(activeStep.component, {
                       currentStep: step + 1,
                       totalSteps: steps.length,
