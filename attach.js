3c534dc73d98e67384e57c85e6f2ac434cc4d76b
Zach Silveira <zackify@gmail.com>
2018-10-17 09:48:14 -0700

    wait on layout to finish before placing step

--- attach.js
+++ attach.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { View } from 'react-native';
 import { Consumer } from './context';
 
+let timer;
+
 export default class Attach extends React.Component {
   render() {
     const { name, children, style, viewStyle } = this.props;
@@ -16,11 +18,29 @@ export default class Attach extends React.Component {
                 this.marker = ref;
               }}
               style={viewStyle}
-              onLayout={() =>
-                setTimeout(
+              onLayout={() => {
+                // make sure we only render the tour
+                // after elements stop moving, ex route transition
+
+                timer = setInterval(
                   () =>
                     this.marker &&
                     this.marker.measure((x, y, width, height, pageX, pageY) => {
+                      if (
+                        this.x === x &&
+                        this.y === y &&
+                        this.pageX === pageX &&
+                        this.pageY === pageY
+                      )
+                        clearInterval(timer);
+                      else {
+                        this.x = x;
+                        this.y = y;
+                        this.pageX = pageX;
+                        this.pageY = pageY;
+                        return;
+                      }
+
                       const circleSize = height > width ? height : width;
 
                       onLayout(name, {
@@ -38,9 +58,9 @@ export default class Attach extends React.Component {
                         overlay: children,
                       });
                     }),
-                  step.beforeDelay || 0,
-                )
-              }
+                  250,
+                );
+              }}
             >
               {children}
             </View>
